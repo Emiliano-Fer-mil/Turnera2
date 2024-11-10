bf2b020353aa4c564a0b34ff00a9fa681b06986f
Emiliano-Fer-mil <emilianofernandezmilki@gmail.com>
2024-11-10 03:12:59 -0300

    Agregar archivos de proyecto.

---
+++ Turnera2/Models/ClinicaContexto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turnera2.Models;
+
+namespace Turnera2.Models
+{
+    public class ClinicaContexto : DbContext
+    {
+        public ClinicaContexto(DbContextOptions<ClinicaContexto> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-HPE7L6H\SQLEXPRESS;Database=CLINICA;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        
+
+    }
+}
---
+++ Turnera2/Models/Ticket.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Turnera2.Models
+{
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TicketId { get; set; }
+        public DateOnly Fecha { get; set; }
+        public TimeOnly Hora { get; set; }
+        [ForeignKey("MedicoId")]
+        public Medico Medico { get; set; }
+        [ForeignKey("PacienteId")]
+        public Paciente Paciente  { get; set; }
+
+        /*
+        public override string ToString()
+        {
+            Medico medico = AccesoADatos.BuscarMedico(medicoId);
+            Paciente paciente = AccesoADatos.BuscarPaciente(pacienteId);
+            return ($"-------------------------------------------- \n" +
+                $"Turno: {fecha} / {hora} \n" +
+                $"Especialidad: {Enum.GetName(typeof(Especialidad),medico.Especialidad)}\n" +
+                $"Dr. {medico.NombreCompleto}\n" +
+                $"Paciente: {paciente.NombreCompleto}\n" +
+                $"Cobertura: {Enum.GetName(typeof(ObraSocial), paciente.ObraSocial)}\n");
+        }
+        */
+    }
+}
